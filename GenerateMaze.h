ae66010a4d03f3951bd4e9b06c6bcc9042b99852
My Name <myemail@example.com>
2024-10-17 15:21:55 +1100

    GenerateMazeChanges

---
+++ GenerateMaze.h
@@ -0,0 +1,30 @@
+#ifndef GENERATE_MAZE_H
+#define GENERATE_MAZE_H
+
+#include <vector>
+#include <random>
+#include <iostream>
+#include <algorithm>
+
+class GenerateMaze {
+
+public:
+
+GenerateMaze(int x, int y);
+void UserInputMaze(int x, int y);
+// void GenerateRandomMaze(int x, int y);
+// void carveMaze();
+// int randomMove(int x, int y);
+// bool isValid(int x, int y, int dirX, int dirY);
+void printMaze();
+
+
+private:
+std::vector<std::vector<char>> maze;
+int currLoc[2];
+int lastDecision[2];
+std::vector<std::vector<int[2]>> pathWay; 
+
+};
+
+#endif
