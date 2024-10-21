4a8297aa7a6a7cb162e4808deb05f22b73cd44b5
My Name <myemail@example.com>
2024-10-21 14:12:05 +1100

    Including random generation of maze

--- GenerateMaze.h
+++ GenerateMaze.h
@@ -5,33 +5,37 @@
 #include <random>
 #include <iostream>
 #include <algorithm>
+#include <array>
+#include <mcpp/mcpp.h>
+#include "Maze.h"
+#include "env.h"
 
 class GenerateMaze {
 
 public:
 
-GenerateMaze(int x, int y);
-// ~GenerateMaze();
-// GenerateMazze(int x, int y);  // Constructor
-// ~GenerateMazze();             // Destructor
+GenerateMaze();
+GenerateMaze(int x, int y, mcpp::Coordinate currCord);
+void GenerateRandMaze();
+void carveMaze();
+int randomMove(int x, int y);
+bool isValid(int x, int y, int dirX, int dirY);
 void UserInputMaze(int x, int y);
-// void GenerateRandomMaze(int x, int y);
-// void carveMaze();
-// int randomMove(int x, int y);
-// bool isValid(int x, int y, int dirX, int dirY);
-// void printMaze();
+void printMaze();
 int getMazeHeight();
 int getMazeWidth();
+char** getMaze() const;
+mcpp::Coordinate getCord();
+
 
 
 private:
-//std::vector<std::vector<char>> maze;
-char** maze;
-int currLoc[2];
-int lastDecision[2];
-std::vector<std::vector<int[2]>> pathWay; 
+std::vector<std::vector<char>> maze;
+std::array<int, 2> currLoc{};
+std::vector<std::array<int, 2>> pathWay;
 int width;
 int height;
+mcpp::Coordinate cord;
 
 };
 
