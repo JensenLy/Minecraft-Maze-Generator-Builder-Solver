23671738a7361e8a6eb9a9957acc30d252797dc2
My Name <myemail@example.com>
2024-10-28 10:47:10 +1100

    Including the validation and test mode in mazeRunner

--- mazeRunner.cpp
+++ mazeRunner.cpp
@@ -39,10 +39,7 @@ int main(int argc, char* argv[]){
     std::string userInput;
     // maze* =nullptr
     Maze *m = new Maze();
-    int userX;
-    int userY;
     bool correctInput = false;
-    mcpp::Coordinate playerOrg;
     GenerateMaze userMaze;
     //Maze
 
@@ -77,74 +74,55 @@ int main(int argc, char* argv[]){
             printGenerateMazeMenu();
             getline(std::cin, userInput);
             
-            if(userInput == "1"){
-
-                std::cout << "In Minecraft, navigate to where you need the maze\nto be built in Minecraft and type - done:" << std::endl;
-
-                std::string input; 
-                std::cin >> input;
-                if (input == "done"){
-
-                    playerOrg = mc.getPlayerPosition();
-
-                    std::cout << "Enter the length and width of maze" << std::endl;
-
-                    while(!correctInput){
-                        std::cin >> userX >> userY;
-                
-                        if(userX%2 == 0 || userY%2 == 0){
-                            std::cout << "Please enter odd values for the length and width of maze" << std::endl;
-                        }
-                        else{
-                            correctInput = true;
-                        }
-                    }
-
-                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-
-                userMaze = GenerateMaze(userX, userY, playerOrg);
-                userMaze.UserInputMaze(userX, userY);
-                userMaze.printMaze();
-                
+            if(mode){
+                    if(userInput == "1"){
+                    userMaze = userMaze.ValidateUserMazeSize();
+                    userMaze.ValidateUserMazeInput();
+                    userMaze.printMaze();
                 }
-
-            }
-            else if(userInput == "2"){
-                std::cout << "Enter the length and width of maze" << std::endl;
-
-                while(!correctInput){
-                    std::cin >> userX >> userY;
-            
-                    if(userX%2 == 0 || userY%2 == 0){
-                        std::cout << "Please enter odd values for the length and width of maze" << std::endl;
-                    }
-                    else{
-                        correctInput = true;
-                    }
+                else if(userInput == "2"){
+                    userMaze = userMaze.ValidateUserMazeSize();
+                    std::cout << "***Printing Maze***" << std::endl;
+                    std::cout << "Base Point: " << userMaze.getCord() << std::endl;
+                    std::cout << "Structure: "  << std::endl;
+                    userMaze.GenerateTestMaze();
+                    userMaze.carveTestMaze();
+                    userMaze.printMaze();
                 }
-
-                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-
-                playerOrg = mc.getPlayerPosition();
-                userMaze = GenerateMaze(userX, userY, playerOrg);
-                std::cout << "***Printing Maze***" << std::endl;
-                std::cout << "Base Point: " << userMaze.getCord() << std::endl;
-                std::cout << "Structure: " << userMaze.getCord() << std::endl;
-                userMaze.GenerateRandMaze();
-                userMaze.carveMaze();
-                userMaze.printMaze();
-
-            }
-            else if(userInput == "3"){
-                curState = ST_Main;
+                else if(userInput == "3"){
+                    curState = ST_Main;
+                }
+                else{
+                    std::cout << "Please select menu item with numbers 1 to 3"; 
+                }
             }
             else{
-                std::cout << "Please select menu item with numbers 1 to 3"; 
+                if(userInput == "1"){
+                    userMaze = userMaze.ValidateUserMazeSize();
+                    userMaze.ValidateUserMazeInput();
+                    userMaze.printMaze();
+                }
+                else if(userInput == "2"){
+                    userMaze = userMaze.ValidateUserMazeSize();
+                    std::cout << "***Printing Maze***" << std::endl;
+                    std::cout << "Base Point: " << userMaze.getCord() << std::endl;
+                    std::cout << "Structure: "  << std::endl;
+                    userMaze.GenerateRandMaze();
+                    userMaze.carveMaze();
+                    userMaze.printMaze();
+                }
+                else if(userInput == "3"){
+                    curState = ST_Main;
+                }
+                else{
+                    std::cout << "Please select menu item with numbers 1 to 3"; 
+                }
             }
+
         }
         else if(curState == ST_Creators){
             std::cout << "Building maze" << std::endl;
-            m = new Maze(playerOrg, userX, userY, correctInput, userMaze.getMaze());
+            m = new Maze(userMaze.getCord(), userMaze.getMazeWidth(), userMaze.getMazeHeight(), correctInput, userMaze.getMaze());
 
             m->scanTerrain();
             m->storeTerrain();
@@ -160,7 +138,7 @@ int main(int argc, char* argv[]){
             if (mode) { // Test mode
                 if(userInput == "1") {
                     Agent* solve = new Agent(); 
-                    solve->manualSolveTest(playerOrg, userMaze.getMazeHeight(), userMaze.getMazeWidth(), userMaze.getMaze()); 
+                    solve->manualSolveTest(userMaze.getCord(), userMaze.getMazeHeight(), userMaze.getMazeWidth(), userMaze.getMaze()); 
                     delete solve;
                 }
                 else if(userInput == "2"){
@@ -183,7 +161,7 @@ int main(int argc, char* argv[]){
             else { // Normal mode
                 if(userInput == "1"){ 
                     Agent* solve = new Agent(); 
-                    solve->manualSolve(playerOrg, userMaze.getMazeHeight(), userMaze.getMazeWidth(), userMaze.getMaze()); 
+                    solve->manualSolve(userMaze.getCord(), userMaze.getMazeHeight(), userMaze.getMazeWidth(), userMaze.getMaze()); 
                     delete solve; 
                 }
                 else if(userInput == "2"){
